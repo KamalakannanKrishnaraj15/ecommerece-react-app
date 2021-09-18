b45808dfa56681e81e4825ad51674780d498c8a7
kamal <kamal@197BLRMB28128.local>
2021-09-18 16:46:29 +0530

    user redirection after successful sign-in is taken care

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import lottie from 'lottie-web';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router'
 
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
@@ -10,6 +11,7 @@ import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = ({ currentUser }) => {
   const [isWelcomeVisible, setWelcomeVisible] = useState(null);
+  const [redirect, setRedirect] = useState(null);
   const [isShoppingLoaderVisible, setShoppingLoaderVisible] = useState(false);
 
   const handleSubmit = (value) => {
@@ -17,16 +19,25 @@ const SignInAndSignUpPage = ({ currentUser }) => {
   };
 
   useEffect(() => {
+    setShoppingLoaderVisible(true);
+  }, []);
+
+  useEffect(() => {
     if (currentUser === null) {
+      setRedirect(false);
       setWelcomeVisible(false);
     } else {
       setShoppingLoaderVisible(false);
       setWelcomeVisible(true);
+      setTimeout(() => {
+        setRedirect(true);
+      }, 3000);
     }
   }, [currentUser]);
 
   useEffect(() => {
     const lottieRef = document.getElementById('welcome-lottie');
+    const loadingRef = document.getElementById('continue-shopping');
     lottie.loadAnimation({
       container: lottieRef,
       background: 'white',
@@ -35,33 +46,54 @@ const SignInAndSignUpPage = ({ currentUser }) => {
       loop: true,
       path: 'https://assets2.lottiefiles.com/packages/lf20_47pyyfcf.json',
     });
+    lottie.loadAnimation({
+      container: loadingRef,
+      background: 'white',
+      renderer: 'svg',
+      autoplay: true,
+      loop: true,
+      path: 'https://assets2.lottiefiles.com/packages/lf20_azhtvt8x.json',
+    });
   }, [isWelcomeVisible]);
 
 
   const getComponent = () => {
-    if (isWelcomeVisible) {
-      return (
-        <div className='welcome-msg-container'>
-          <div>
+    switch(true) {
+      case redirect: {
+        return (
+          <Redirect to="/" />
+        );
+      }
+      case isWelcomeVisible: {
+        return (
+          <div className='welcome-msg-container'>
             <div>
-              <div id='welcome-lottie' className='welcome-lottie-anmiation' />
+              <div>
+                <div id='welcome-lottie' className='welcome-lottie-anmiation' />
+              </div>
+              <h2 className='welcome-msg'>Welcome to crown shopping!</h2>
+              <div className='continue-shopping-msg'>
+                Continue shopping
+                <div id='continue-shopping' className='continue-shopping-animation' />
+              </div>
             </div>
-            <h2 className='welcome-msg'>Welcome to crown shopping!</h2>
           </div>
-        </div>
-      );
-    } else if (isShoppingLoaderVisible) {
-      return (
-        <ShoppingLoader state={isShoppingLoaderVisible} />
-      );
-    } else {
-      return (
-        <Fragment>
-          <SignIn onSingInSubmit={handleSubmit} />
-          <SignUp onSingUpSubmit={handleSubmit} />
-        </Fragment>
-      );
-    }  
+        );
+      }
+      case isShoppingLoaderVisible: {
+        return (
+          <ShoppingLoader state={isShoppingLoaderVisible} />
+        );
+      }
+      default: {
+        return (
+          <Fragment>
+            <SignIn onSingInSubmit={handleSubmit} />
+            <SignUp onSingUpSubmit={handleSubmit} />
+          </Fragment>
+        );
+      }
+    };
   };
 
   return (
